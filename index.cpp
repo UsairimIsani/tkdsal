cc90cab6ab5c26d7810a7d872679fef1ecc42c47
Usairim Isani <usairim.isani@outlook.com>
2020-06-02 14:39:23 +0500

    Data types and Type Casting.

--- index.cpp
+++ index.cpp
@@ -1,15 +1,34 @@
 #include <stdio.h>
+#include <iostream>
 using namespace std;
 // Hello World I am Learning C++
 
 int main(int argc, char const *argv[])
 {
-    int some_integer = 21;      // My age HAHA
-    const int girl_friends = 0; //sadly won't change any time sooon.
-    float some_one_on_water = 34.03;
-    double world = 2.34213312e-56;
-    unsigned char character = 'h';
-    unsigned long long int disturbing = 55464456;
+    int num1 = 70;
+    double num2 = 256.783;
+    char ch = 'A';
 
+    cout << num1 << endl;                // print integer
+    cout << num2 << endl;                // print double
+    cout << "character: " << ch << endl; // print char
+    char a;
+    int num;
+
+    cout << "Enter a character and an integer: ";
+    cin >> a >> num; // Need More Info on this
+
+    cout << "Character: " << a << endl;
+    cout << "Number: " << num;
+
+    int hus = 4;
+    double jis = 4.78;
+    double pizz = (double)hus;
+    int ajj = int(jis);
+    cout << endl
+         << hus << endl
+         << jis << endl
+         << pizz << endl
+         << ajj << endl;
     return 0;
 }
