1b60f4b03422dbb502c7347246692939205f24d3
Usairim Isani <usairim.isani@outlook.com>
2020-06-02 10:32:36 +0500

    Project Initialization.

---
+++ index.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+using namespace std;
+// Hello World I am Learning C++
+
+int main(int argc, char const *argv[])
+{
+    int some_integer = 21;      // My age HAHA
+    const int girl_friends = 0; //sadly won't change any time sooon.
+    double world = 2.34213312e-56;
+    printf("Hello world\n %f", world);
+    printf("Hello world\n %f", world);
+    printf("Hello world\n %f", world);
+    return 0;
+}
