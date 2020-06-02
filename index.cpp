fb3082fa9c92cf40d63dce073309b91c521b466c
Usairim Isani <usairim.isani@outlook.com>
2020-06-02 10:50:48 +0500

    Datatypes.

--- index.cpp
+++ index.cpp
@@ -6,9 +6,10 @@ int main(int argc, char const *argv[])
 {
     int some_integer = 21;      // My age HAHA
     const int girl_friends = 0; //sadly won't change any time sooon.
+    float some_one_on_water = 34.03;
     double world = 2.34213312e-56;
-    printf("Hello world\n %f", world);
-    printf("Hello world\n %f", world);
-    printf("Hello world\n %f", world);
+    unsigned char character = 'h';
+    unsigned long long int disturbing = 55464456;
+
     return 0;
 }
