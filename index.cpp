d1783e38ae27e7bf7f451ec117f19e691ca05d19
Usairim Isani <usairim.isani@outlook.com>
2020-06-04 00:00:43 +0500

    By refernce

--- index.cpp
+++ index.cpp
@@ -1,15 +1,29 @@
-using namespace std;
 #include <iostream>
+using namespace std;
+
+// Function prototype
+void swap(int &, int &);
+
 int main()
 {
+    int a = 1, b = 2;
+    cout << "Before swapping" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
 
-    int **p, *q, s;
-    p = &q;
-    q = &s;
-    s = 3;
-    cout << **p << " " << *q << " " << s << " " << endl;
-    cout << p << " " << q << " " << s << " " << endl;
-    cout << *p << " " << *q << " " << &s << " " << endl;
+    swap(a, b);
+
+    cout << "\nAfter swapping" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
 
     return 0;
+}
+
+void swap(int &n1, int &n2)
+{
+    int temp;
+    temp = n1;
+    n1 = n2;
+    n2 = temp;
 }
