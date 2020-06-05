38091d8a01a9411749fcc8a12e220d49b84de07b
Usairim Isani <usairim.isani@outlook.com>
2020-06-05 20:23:59 +0500

    Basic Inheritence.

--- index.cpp
+++ index.cpp
@@ -1,45 +1,51 @@
 #include <iostream>
 using namespace std;
 
-class Test
+class Person
 {
-private:
-    int num;
-    float *ptr;
-
 public:
-    Test()
+    string profession;
+    int age;
+
+    Person() : profession("unemployed"), age(16) {}
+    void display()
     {
-        cout << "Enter total number of students: ";
-        cin >> num;
-
-        ptr = new float[num];
-
-        cout << "Enter GPA of students." << endl;
-        for (int i = 0; i < num; ++i)
-        {
-            cout << "Student" << i + 1 << ": ";
-            cin >> *(ptr + i);
-        }
+        cout << "My profession is: " << profession << endl;
+        cout << "My age is: " << age << endl;
+        walk();
+        talk();
     }
-
-    ~Test()
-    {
-        delete[] ptr;
-    }
-
-    void Display()
-    {
-        cout << "\nDisplaying GPA of students." << endl;
-        for (int i = 0; i < num; ++i)
-        {
-            cout << "Student" << i + 1 << " :" << *(ptr + i) << endl;
-        }
-    }
+    void walk() { cout << "I can walk." << endl; }
+    void talk() { cout << "I can talk." << endl; }
 };
+
+// MathsTeacher class is derived from base class Person.
+class MathsTeacher : public Person
+{
+public:
+    void teachMaths() { cout << "I can teach Maths." << endl; }
+};
+
+// Footballer class is derived from base class Person.
+class Footballer : public Person
+{
+public:
+    void playFootball() { cout << "I can play Football." << endl; }
+};
+
 int main()
 {
-    Test s;
-    s.Display();
+    MathsTeacher teacher;
+    teacher.profession = "Teacher";
+    teacher.age = 23;
+    teacher.display();
+    teacher.teachMaths();
+
+    Footballer footballer;
+    footballer.profession = "Footballer";
+    footballer.age = 19;
+    footballer.display();
+    footballer.playFootball();
+
     return 0;
 }
