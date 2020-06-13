acc6cd6f9b92f147e08cde4c659781353823bb87
Usairim Isani <usairim.isani@outlook.com>
2020-06-13 22:57:52 +0500

    Queues

--- index.cpp
+++ index.cpp
@@ -1,103 +1,107 @@
-// Stack implementation in C++
 
-#include <stdlib.h>
 #include <iostream>
+#define SIZE 5
 
 using namespace std;
 
-#define MAX 10
-int size = 0;
+class Queue {
+   private:
+  int items[SIZE], front, rear;
 
-// Creating a stack
-struct stack
-{
-    int items[MAX];
-    int top;
+   public:
+  Queue() {
+    front = -1;
+    rear = -1;
+  }
+
+  bool isFull() {
+    if (front == 0 && rear == SIZE - 1) {
+      return true;
+    }
+    return false;
+  }
+
+  bool isEmpty() {
+    if (front == -1)
+      return true;
+    else
+      return false;
+  }
+
+  void enQueue(int element) {
+    if (isFull()) {
+      cout << "Queue is full";
+    } else {
+      if (front == -1) front = 0;
+      rear++;
+      items[rear] = element;
+      cout << endl
+         << "Inserted " << element << endl;
+    }
+  }
+
+  int deQueue() {
+    int element;
+    if (isEmpty()) {
+      cout << "Queue is empty" << endl;
+      return (-1);
+    } else {
+      element = items[front];
+      if (front >= rear) {
+        front = -1;
+        rear = -1;
+      } /* Q has only one element, so we reset the queue after deleting it. */
+      else {
+        front++;
+      }
+      cout << endl
+         << "Deleted -> " << element << endl;
+      return (element);
+    }
+  }
+
+  void display() {
+    /* Function to display elements of Queue */
+    int i;
+    if (isEmpty()) {
+      cout << endl
+         << "Empty Queue" << endl;
+    } else {
+      cout << endl
+         << "Front index-> " << front;
+      cout << endl
+         << "Items -> ";
+      for (i = front; i <= rear; i++)
+        cout << items[i] << "  ";
+      cout << endl
+         << "Rear index-> " << rear << endl;
+    }
+  }
 };
-typedef struct stack st;
 
-void createEmptyStack(st *s)
-{
-    s->top = -1;
-}
+int main() {
+  Queue q;
 
-// Check if the stack is full
-int isfull(st *s)
-{
-    if (s->top == MAX - 1)
-        return 1;
-    else
-        return 0;
-}
+  //deQueue is not possible on empty queue
+  q.deQueue();
 
-// Check if the stack is empty
-int isempty(st *s)
-{
-    if (s->top == -1)
-        return 1;
-    else
-        return 0;
-}
+  //enQueue 5 elements
+  q.enQueue(1);
+  q.enQueue(2);
+  q.enQueue(3);
+  q.enQueue(4);
+  q.enQueue(5);
 
-// Add elements into stack
-void push(st *s, int newitem)
-{
-    if (isfull(s))
-    {
-        printf("STACK FULL");
-    }
-    else
-    {
-        s->top++;
-        s->items[s->top] = newitem;
-    }
-    size++;
-}
+  //6th element can't be added to queue because queue is full
+  q.enQueue(6);
 
-// Remove element from stack
-void pop(st *s)
-{
-    if (isempty(s))
-    {
-        printf("\n STACK EMPTY \n");
-    }
-    else
-    {
-        printf("Item popped= %d", s->items[s->top]);
-        s->top--;
-    }
-    size--;
-    cout << endl;
-}
+  q.display();
 
-// Print elements of stack
-void printStack(st *s)
-{
-    printf("Stack: ");
-    for (int i = 0; i < size; i++)
-    {
-        cout << s->items[i] << " ";
-    }
-    cout << endl;
-}
+  //deQueue removes element entered first i.e. 1
+  q.deQueue();
 
-// Driver code
-int main()
-{
-    int ch;
-    st *s = (st *)malloc(sizeof(st));
+  //Now we have just 4 elements
+  q.display();
 
-    createEmptyStack(s);
-
-    push(s, 1);
-    push(s, 2);
-    push(s, 3);
-    push(s, 4);
-
-    printStack(s);
-
-    pop(s);
-
-    cout << "\nAfter popping out\n";
-    printStack(s);
+  return 0;
 }
